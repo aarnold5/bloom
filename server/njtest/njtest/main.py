80e1a1c0c4ea17e40c1ba7add550fc95ecb90dce
Connor Spencer <Connor.R.Spencer.22@Dartmouth.edu>
2022-03-02 14:36:52 -0500

    Pushed Algorithm to GCloud

--- server/njtest/njtest/main.py
+++ server/njtest/njtest/main.py
@@ -1,29 +1,27 @@
 import pandas as pd
 import numpy as np
-import json
-import re 
-import sys
-import itertools
 
-import os
+#import os
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import pairwise_distances
 import sklearn.metrics.pairwise as pw
 from sklearn.preprocessing import MinMaxScaler
-import matplotlib.pyplot as plt
 
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-from spotipy.oauth2 import SpotifyOAuth
-import spotipy.util as util
+from flask import request, escape
+import functions_framework
 
-import warnings
-warnings.filterwarnings("ignore")
+#import spotipy
+#from spotipy.oauth2 import SpotifyClientCredentials
+#from spotipy.oauth2 import SpotifyOAuth
+#import spotipy.util as util
+
+#import warnings
+#warnings.filterwarnings("ignore")
 
 #simple function to create OHE features
 def ohe_prep(df, column, new_name): 
@@ -85,24 +83,13 @@ def create_feature_set(df, float_cols):
     
     return final
 
-def load_data(filename):
+def load_data(db):
 
-    #Firebase stuff
-    project_id = "bloom-838b5"
-    cred = credentials.ApplicationDefault()
-    firebase_admin.initialize_app(cred, {'projectId': project_id})
-
-    db = firestore.client()
     docs = db.collection(u'songs').where(u'duration_ms', u'>=', 0).stream()
 
     spotify_df = pd.DataFrame()
     for doc in docs:
         spotify_df = spotify_df.append(doc.to_dict(), ignore_index = True)
-
-    #dir_path = os.path.dirname(os.path.realpath(__file__))
-    #spotify_df = pd.read_json(dir_path + '/' + filename + '.json', orient='split')
-    #spotify_df = pd.read_csv(dir_path + '/' + filename + '.csv')
-    #print(spotify_df.head())
 
     return spotify_df
 
@@ -145,7 +132,7 @@ def authenticate_to_spotify():
 
     return sp, id_name, list_photo
 
-def create_necessary_outputs(playlist_name, id_dic, df):
+def create_necessary_outputs(playlist_ids, df):
     """ 
     Pull songs from a specific playlist.
 
@@ -155,32 +142,37 @@ def create_necessary_outputs(playlist_name, id_dic, df):
         df (pandas dataframe): spotify dataframe
         
     Returns: 
-        playlist: all songs in the playlist THAT ARE AVAILABLE IN THE KAGGLE DATASET
+        playlist: all songs in the playlist THAT ARE AVAILABLE IN THE DATASET
     """
     
     #generate playlist dataframe
+
     playlist = pd.DataFrame()
-    playlist_name = playlist_name
+    playlist_ids = playlist_ids.split("[")[1].split("]")[0].split(",")
+    for song in playlist_ids:
+        song = song.replace("\"", "")
+        if df['id'].str.contains(song).any():
+            playlist = playlist.append({'id': song}, ignore_index = True)
 
     #ensure there are no songs in the playlist
-    songs = sp.playlist(id_dic[playlist_name])['tracks']['items']
-    if len(songs) == 0:
-        print("Error: Playlist contains no songs. Try adding one or more songs to the playlist, then try again.")
-        return None
+    #songs = sp.playlist(id_dic[playlist_name])['tracks']['items']
+    #if len(songs) == 0:
+        #print("Error: Playlist contains no songs. Try adding one or more songs to the playlist, then try again.")
+        #return None
 
     #if there are songs, adds information to the playlist dataframe
-    for ix, i in enumerate(songs):
+    #for ix, i in enumerate(songs):
         #print(i['track']['artists'][0]['name'])
-        playlist.loc[ix, 'artist'] = i['track']['artists'][0]['name']
-        playlist.loc[ix, 'name'] = i['track']['name']
-        playlist.loc[ix, 'id'] = i['track']['id']
-        playlist.loc[ix, 'url'] = i['track']['album']['images'][1]['url']
-        playlist.loc[ix, 'date_added'] = i['added_at']
+        #playlist.loc[ix, 'artist'] = i['track']['artists'][0]['name']
+        #playlist.loc[ix, 'name'] = i['track']['name']
+        #playlist.loc[ix, 'id'] = i['track']['id']
+        #playlist.loc[ix, 'url'] = i['track']['album']['images'][1]['url']
+        #playlist.loc[ix, 'date_added'] = i['added_at']
 
-    playlist['date_added'] = pd.to_datetime(playlist['date_added'])
+    #playlist['date_added'] = pd.to_datetime(playlist['date_added'])
     #print(playlist.to_string())
     
-    playlist = playlist[playlist['id'].isin(df['id'].values)]#.sort_values('date_added',ascending = False)
+    #playlist = playlist[playlist['id'].isin(df['id'].values)]#.sort_values('date_added',ascending = False)
     #print(playlist.to_string())
 
     return playlist
@@ -200,21 +192,21 @@ def generate_playlist_feature(complete_feature_set, playlist_df, weight_factor):
     """
     
     complete_feature_set_playlist = complete_feature_set[complete_feature_set['id'].isin(playlist_df['id'].values)]
-    complete_feature_set_playlist = complete_feature_set_playlist.merge(playlist_df[['id','date_added']], on = 'id', how = 'inner')
+    complete_feature_set_playlist = complete_feature_set_playlist.merge(playlist_df[['id']], on = 'id', how = 'inner')
     complete_feature_set_nonplaylist = complete_feature_set[~complete_feature_set['id'].isin(playlist_df['id'].values)]
     
-    playlist_feature_set = complete_feature_set_playlist.sort_values('date_added',ascending=False)
+    playlist_feature_set = complete_feature_set_playlist#.sort_values('date_added',ascending=False)
 
-    most_recent_date = playlist_feature_set.iloc[0,-1]
+    #most_recent_date = playlist_feature_set.iloc[0,-1]
     
-    for ix, row in playlist_feature_set.iterrows():
-        playlist_feature_set.loc[ix,'months_from_recent'] = int((most_recent_date.to_pydatetime() - row.iloc[-1].to_pydatetime()).days / 30)
+    #for ix, row in playlist_feature_set.iterrows():
+        #playlist_feature_set.loc[ix,'months_from_recent'] = int((most_recent_date.to_pydatetime() - row.iloc[-1].to_pydatetime()).days / 30)
         
-    playlist_feature_set['weight'] = playlist_feature_set['months_from_recent'].apply(lambda x: weight_factor ** (-x))
+    playlist_feature_set['weight'] = np.ones(len(playlist_feature_set))
     
     playlist_feature_set_weighted = playlist_feature_set.copy()
-    playlist_feature_set_weighted.update(playlist_feature_set_weighted.iloc[:,:-4].mul(playlist_feature_set_weighted.weight,0))
-    playlist_feature_set_weighted_final = playlist_feature_set_weighted.iloc[:, :-4]
+    playlist_feature_set_weighted.update(playlist_feature_set_weighted.iloc[:,:-2].mul(playlist_feature_set_weighted.weight,0))
+    playlist_feature_set_weighted_final = playlist_feature_set_weighted.iloc[:, :-2]
     #print(playlist_feature_set_weighted_final.to_string())
     
     return playlist_feature_set_weighted_final.sum(axis = 0), complete_feature_set_nonplaylist
@@ -239,7 +231,7 @@ def generate_playlist_recos(df, features, nonplaylist_features, top_many):
     non_playlist_df['sim'] = pw.cosine_distances(nonplaylist_features.drop('id', axis = 1).values, features.values.reshape(1, -1))[:,0]
     non_playlist_df_top_x = non_playlist_df.sort_values('sim',ascending = True).head(top_many)
     #print(non_playlist_df_top_x)
-    non_playlist_df_top_x['url'] = non_playlist_df_top_x['id'].apply(lambda x: sp.track(x)['album']['images'][1]['url'])
+    #non_playlist_df_top_x['url'] = non_playlist_df_top_x['id'].apply(lambda x: sp.track(x)['album']['images'][1]['url'])
     #print(non_playlist_df_top_x.to_string())
     
     return non_playlist_df_top_x
@@ -253,29 +245,75 @@ def extract_artists(artist_dict):
             str += ", "
     return str
 
-def output_recommendations(recs):
+def output_recommendations(recs, sp):
     i = 0
     for rec in recs.iterrows():
         i += 1
         print(str(i) + ". " + sp.track(rec[1]['id'])['name'] + " by " + extract_artists(sp.track(rec[1]['id'])['artists']))
 
-
 pd.set_option('display.max_columns', None)
-pname = "Bloom Algorithm Testing"
+#pname = "Bloom Algorithm Testing"
 
-spotify_df = load_data("test2")
-float_cols = np.concatenate([spotify_df.dtypes[spotify_df.dtypes == 'float64'].index.values,spotify_df.dtypes[spotify_df.dtypes == 'int64'].index.values])
-spotify_df, complete_feature_set = perform_feature_engineering(spotify_df, float_cols)
-sp, id_name, list_photo = authenticate_to_spotify()
-for i in range(1,2):
-    test_playlist = create_necessary_outputs(pname, id_name, spotify_df)
+def perform_analysis(pname):
+    
+    #print(pname.split("[")[1].split("]")[0].split(","))
+    #asd
+    #Firebase stuff
+    project_id = "bloom-838b5"
+    cred = credentials.ApplicationDefault()
+    firebase_admin.initialize_app(cred, {'projectId': project_id})
+    db = firestore.client()
+    spotify_df = load_data(db)
+    float_cols = np.concatenate([spotify_df.dtypes[spotify_df.dtypes == 'float64'].index.values,spotify_df.dtypes[spotify_df.dtypes == 'int64'].index.values])
+    spotify_df, complete_feature_set = perform_feature_engineering(spotify_df, float_cols)
+    #sp, id_name, list_photo = authenticate_to_spotify()
+    test_playlist = create_necessary_outputs(pname, spotify_df)
     complete_feature_set_playlist_vector, complete_feature_set_nonplaylist = generate_playlist_feature(complete_feature_set, test_playlist, 1)
     recs_top10 = generate_playlist_recos(spotify_df, complete_feature_set_playlist_vector, complete_feature_set_nonplaylist, 3)
-    print(recs_top10['id'])
-    output_recommendations(recs_top10)
-    #Adds the top song to the user's playlist, and plays the last one
-    sp.playlist_add_items(id_name[pname], recs_top10.head(1)['id'], position=sp.playlist(id_name[pname])['tracks']['total'])
-    #if (i == 9):
-        #sp.add_to_queue(recs_top10.head(1)['id'].iloc[0])
-        #sp.start_playback()
-        #sp.next_track()
+    return recs_top10['id']
+
+@functions_framework.http
+def algorithm(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    """
+
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    if request_json and 'songs' in request_json:
+        pname = request_json['songs']
+    elif request_args and 'songs' in request_args:
+        pname = request_args['songs']
+    else:
+        pname = ''
+
+    recos = perform_analysis(pname)
+    req = "{\"songs\":["
+    for id in range(len(recos) - 1):
+        req = req + "\"" + recos[id] + "\","
+    req = req + "\"" + recos[len(recos)-1] + "\"]}"
+    return '{}'.format(escape(req))
+
+@functions_framework.http
+def hello_http(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    """
+
+    return 'Hello World!'
+
+#print(perform_analysis("{\"songs\"=[\"05JdgtCKkZ5CoOjM5KS4EG\",\"0EH7sgeiFqDa3eS7ieW2zs\",\"0SLtqCrXBRrnkxSOMA3X4W\"]}"))
+#hello_http(flask.Request)

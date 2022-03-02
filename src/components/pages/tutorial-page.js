b3f15d1c57db747066f71636c2842b05253c4ff3
aarnold5 <alaina.j.arnold.22@dartmouth.edu>
2022-03-02 05:37:12 -0500

    deleting unnecessary comments

--- src/components/pages/tutorial-page.js
+++ src/components/pages/tutorial-page.js
@@ -20,48 +20,6 @@ class TutorialPage extends Component {
       trees: [],
       playlist: [],
       searchSuggestions: [],
-      /* playlist: iMap({
-        song1: {
-          title: '',
-          albumCover: 'https://media.architecturaldigest.com/photos/5890e88033bd1de9129eab0a/1:1/w_870,h_870,c_limit/Artist-Designed%20Album%20Covers%202.jpg',
-          thingToPassToFunctionToPlayTheSong: '',
-        },
-        song2: {
-          title: '',
-          albumCover: 'https://i.mdel.net/i/db/2019/12/1255378/1255378-800w.jpg',
-          thingToPassToFunctionToPlayTheSong: '',
-        },
-        song3: {
-          title: '',
-          albumCover: 'https://media2.wnyc.org/i/620/620/l/80/1/dark_side_moonLARGE.jpg',
-          thingToPassToFunctionToPlayTheSong: '',
-        },
-        song4: {
-          title: '',
-          albumCover: 'https://media.architecturaldigest.com/photos/5890e88033bd1de9129eab0a/1:1/w_870,h_870,c_limit/Artist-Designed%20Album%20Covers%202.jpg',
-          thingToPassToFunctionToPlayTheSong: '',
-        },
-        song5: {
-          title: '',
-          albumCover: 'https://i.mdel.net/i/db/2019/12/1255378/1255378-800w.jpg',
-          thingToPassToFunctionToPlayTheSong: '',
-        },
-        song6: {
-          title: '',
-          albumCover: 'https://media2.wnyc.org/i/620/620/l/80/1/dark_side_moonLARGE.jpg',
-          thingToPassToFunctionToPlayTheSong: '',
-        },
-        song7: {
-          title: '',
-          albumCover: 'https://i.mdel.net/i/db/2019/12/1255378/1255378-800w.jpg',
-          thingToPassToFunctionToPlayTheSong: '',
-        },
-        song8: {
-          title: '',
-          albumCover: 'https://media2.wnyc.org/i/620/620/l/80/1/dark_side_moonLARGE.jpg',
-          thingToPassToFunctionToPlayTheSong: '',
-        },
-      }), */
 
       currTree: {
         title: 'Tree 1',
@@ -104,25 +62,9 @@ class TutorialPage extends Component {
     this.search = debounce(this.search, 300);
   }
 
-  /* componentDidMount() {
-    db.fetchTrees((trees) => {})
-      .then((response) => {
-        console.log(response);
-      });
-    // this.setState({ trees: iMap(trees.trees) });
-  } */
-
-  /* componentDidMount() {
-    db.printTrees((trees) => {
-      console.log(trees);
-      // this.setState({ trees: iMap(trees.trees) });
-    });
-  } */
-
   componentDidMount() {
     db.fetchTrees()
       .then((result) => this.setState({ trees: result.trees }));
-    // this.setState({ trees: iMap(trees.trees) });
     db.fetchPlaylist()
       .then((result) => this.setState({ playlist: result.playlist }));
   }
--- src/components/tutorial-components/search-suggestions.js
+++ src/components/tutorial-components/search-suggestions.js
@@ -1,39 +1,14 @@
 import React, { Component } from 'react';
-// import debounce from 'lodash.debounce';
-// import { bloomSearch } from './search';
 import SearchBar from './search-bar';
 import Suggestion from './suggestion';
-// import Suggestion from './suggestion';
 
 class SearchSuggestions extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      // searchSuggestions: [],
-      // selectedSuggestion: null,
     };
-
-    // this.search = debounce(this.search, 300);
   }
-
-  /* search = (text) => {
-    bloomSearch(text).then((searchSuggestions) => {
-      this.setState({
-        searchSuggestions,
-        // selectedSuggestion: searchSuggestions[0],
-      });
-    });
-  }; */
-
-  /* onInputChange = (event) => {
-    this.setState({ searchterm: event.target.value });
-    // console.log(event.target.value);
-    bloomSearch(event.target.value)
-      .then((result) => this.setState({ searchSuggestions: result }));
-    // .then((result) => console.log(result));
-    // console.log(this.state.searchSuggestions);
-  }; */
 
   renderSearchSuggestions() {
     if (this.props.searchSuggestions.length > 0) {
@@ -43,17 +18,6 @@ class SearchSuggestions extends Component {
       return sugList;
     }
     return <div />;
-
-    /* if (this.props.searchSuggestions.length !== 0) {
-      this.props.searchSuggestions.map((suggestion) => {
-        return (
-          <div className="searchSuggestion"><img alt={suggestion.name} src={suggestion.album_cover} /><p>{suggestion.name}</p></div>
-          // <div>{console.log(suggestion.name)}</div>
-          // <Suggestion name={suggestion.name}>{console.log(suggestion.name)}</Suggestion>
-        );
-      });
-    }
-    return <div />; */
   }
 
   render() {

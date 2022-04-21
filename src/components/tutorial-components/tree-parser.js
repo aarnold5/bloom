d85ae2c5147ce4508454b8f19b2e9f24e2db82ee
aarnold5 <alaina.j.arnold.22@dartmouth.edu>
2022-04-21 17:33:55 -0400

    tree edges and album images look good!

--- src/components/tutorial-components/tree-parser.js
+++ src/components/tutorial-components/tree-parser.js
@@ -1,127 +1,130 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable func-names */
+/* eslint-disable no-param-reassign */
+/* eslint-disable camelcase */
+/* eslint-disable no-undef */
+/* eslint-disable no-return-assign */
 // pulls values of or json structure as a list
-to_list = (tree, depth) =>{
-    // SET VALUE YOU WANT
-    var value = tree.root.song;
-    if (!tree.left || !tree.right){
-        if (!tree.left && !tree.right){
-            return [value];
-        }
-        else if (!tree.left){
-            return [value, ...to_list(tree.right, depth+=1)]
-        }
-        else {
-            return [value, ...to_list(tree.left, depth+=1)]
-        }
+to_list = (tree, depth) => {
+  // SET VALUE YOU WANT
+  const value = tree.root.song;
+  if (!tree.left || !tree.right) {
+    if (!tree.left && !tree.right) {
+      return [value];
+    } else if (!tree.left) {
+      return [value, ...to_list(tree.right, depth += 1)];
+    } else {
+      return [value, ...to_list(tree.left, depth += 1)];
     }
-    return [value, ...to_list(tree.left, depth+=1), ...to_list(tree.right, depth+=1)];
-}
+  }
+  return [value, ...to_list(tree.left, depth += 1), ...to_list(tree.right, depth += 1)];
+};
 
-find_song = (search, tree) =>{
-    if (!tree){
-        return null;
-    }
-    if (tree.root.song === search){
-        return tree.root;
-    }
-    var res1=find_song(search, tree.left);
-    var res2 = find_song(search, tree.right);
-    if (res1) return res1;
-    if (res2) return res2;
+find_song = function (search, tree) {
+  if (!tree) {
     return null;
- }
+  }
+  if (tree.root.song === search) {
+    return tree.root;
+  }
+  const res1 = find_song(search, tree.left);
+  const res2 = find_song(search, tree.right);
+  if (res1) return res1;
+  if (res2) return res2;
+  return null;
+};
 
- set_song = (new_song, tree, old_song) => {
-     var oldKey=old_song.song;
-     if (!tree){
-         return null;
-     }
-     else if (tree.root.song === oldKey){
-         tree.root = new_song;
-         return tree;
-     }
-     else {
-         return {root:tree.root, left:set_song(new_song,tree.left,old_song), right:set_song(new_song,tree.right,old_song)};
-     }
- }
+set_song = function (new_song, tree, old_song) {
+  const oldKey = old_song.song;
+  if (!tree) {
+    return null;
+  } else if (tree.root.song === oldKey) {
+    tree.root = new_song;
+    return tree;
+  } else {
+    return { root: tree.root, left: set_song(new_song, tree.left, old_song), right: set_song(new_song, tree.right, old_song) };
+  }
+};
 
- change_attribute = (attr, tree, target) => {
-    if (tree === null){
-        return;
+change_attribute = (attr, tree, target) => {
+  if (tree === null) {
+    return;
+  }
+  if (tree.root === target) {
+    tree.root.attribute = attr;
+  }
+  change_attribute(attr, tree.left, target);
+  change_attribute(attr, tree.right, target);
+};
+
+// cannot delete root
+delete_node = (tree, target) => {
+  if (tree) {
+    if (tree.left.root === target) {
+      tree.left = null;
+    } else if (tree.right.root === target) {
+      tree.right = null;
+    } else {
+      delete_node(tree.left, target);
+      delete_node(tree.right, target);
     }
-    if (tree.root === target){
-        tree.root.attribute = attr;
-    }
-    change_attribute(attr, tree.left, target);
-    change_attribute(attr, tree.right, target);  
- }
+  }
+};
 
-//cannot delete root
-delete_node = (tree, target) =>{
-    if (tree){
-        if (tree.left.root===target) {
-            tree.left=null;
-        }
-        else if (tree.right.root===target){
-            tree.right=null;
-        }
-        else {
-            delete_node(tree.left, target);
-            delete_node(tree.right, target);
-        }
-    }
-}
-
-
-
-
-var t = {root:
-{song: "jazz",
-artist:"blah",
-attribute: "genre",},
-left:{
-    root:{
-        song:"jesus etc.",
-        artist:"wilco",
-        attribute: "genre",
+const t = {
+  root:
+{
+  song: 'jazz',
+  artist: 'blah',
+  attribute: 'genre',
+},
+  left: {
+    root: {
+      song: 'jesus etc.',
+      artist: 'wilco',
+      attribute: 'genre',
     },
     left: null,
     right: null,
-},
-right:{
-    root:{
-        song:"Heads Will Roll",
-        artist:"yeah yeah yeahs",
-        attribute: "mood",
+  },
+  right: {
+    root: {
+      song: 'Heads Will Roll',
+      artist: 'yeah yeah yeahs',
+      attribute: 'mood',
     },
     left: {
-        root:{
-            song:"I wanna hold your hand",
-            artist:"beatles",
-            attribute: "tempo",
-        },
-        left: null,
-        right: null,
+      root: {
+        song: 'I wanna hold your hand',
+        artist: 'beatles',
+        attribute: 'tempo',
+      },
+      left: null,
+      right: null,
     },
     right: null,
-},
+  },
 };
 
-var O_song = {
-    song:"I wanna hold your hand",
-    artist:"beatles",
-    attribute: "tempo",}
+const O_song = {
+  song: 'I wanna hold your hand',
+  artist: 'beatles',
+  attribute: 'tempo',
+};
 
-var O_song2 = {
-    song:"gfah",
-    artist:"beatles",
-    attribute: "tempo",}
+const O_song2 = {
+  song: 'gfah',
+  artist: 'beatles',
+  attribute: 'tempo',
+};
 
-var new_song = {
-    song:"Yellow submarine",
-    artist:"beatles",
-    attribute: "tempo",}
-console.log('%o',to_list(t,1));
-console.log('%o',find_song('fghjk', t));
-console.log('%o',to_list(set_song(new_song, t, O_song),1));
-change_attribute("crazy", t, new_song);
-console.log('%o',t);
+const new_song = {
+  song: 'Yellow submarine',
+  artist: 'beatles',
+  attribute: 'tempo',
+};
+console.log('%o', to_list(t, 1));
+console.log('%o', find_song('fghjk', t));
+console.log('%o', to_list(set_song(new_song, t, O_song), 1));
+change_attribute('crazy', t, new_song);
+console.log('%o', t);
--- src/components/tutorial-components/tree.js
+++ src/components/tutorial-components/tree.js
@@ -1,101 +1,106 @@
+/* eslint-disable max-len */
+/* eslint-disable no-useless-constructor */
 /* eslint-disable react/no-unused-class-component-methods */
 /* eslint-disable no-plusplus */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/no-access-state-in-setstate */
 /* eslint-disable class-methods-use-this */
-import React from "react";
+import React from 'react';
 import Xarrow, { useXarrow, Xwrapper } from 'react-xarrows';
-import Draggable from "react-draggable";
-import TreeNode from "./tree-node";
+import Draggable from 'react-draggable';
+import TreeNode from './tree-node';
 
-
-const TreeNode2 = ({id}) => {
-    const updateXarrow = useXarrow();
-    return (
-        <Draggable onDrag={updateXarrow} onStop={updateXarrow}>
-            <span id={id} className="dot"><img src={'/Users/clairecollins/Desktop/CS/bloom/src/favicon.png'} draggable="false" alt="temp" className="round-img" /></span>
-        </Draggable>
-    );
-  };
+function TreeNode2({ id, album }) {
+  const updateXarrow = useXarrow();
+  return (
+    <Draggable onDrag={updateXarrow} onStop={updateXarrow}>
+      <span id={id} className="dot"><img src={album} draggable="false" alt="temp" className="round-img" /></span>
+    </Draggable>
+  );
+}
 
 class Tree extends React.Component {
-    constructor(props) {
-        super(props);
-      }
-      
-      getEdges(tree, l, t, i, pref){
-        const inputstyle = {position: 'absolute',
-            top: t,
-            left: l,
-        };
-        if(tree){
-            if (pref){
-                console.log("pref works");
-                if(tree.root.rec){
-                    return <div><div style={inputstyle}><TreeNode2 id={tree.root.name}/></div> <Xarrow start={pref} end={tree.root.name} zIndex={3} showHead={false} dashness={true} color={'gray'}/> {this.getEdges(tree.left, l-i, t+150,i/2, tree.root.name)} {this.getEdges(tree.right, l+i,t+150,i/2,tree.root.name)}</div>;
-                }
-                else {
-                    return  <div><div style={inputstyle}><TreeNode2 id={tree.root.name}/></div> <Xarrow start={pref} end={tree.root.name} zIndex={3} showHead={false} color={'#637B47'}/> {this.getEdges(tree.left, l-i, t+150,i/2, tree.root.name)} {this.getEdges(tree.right, l+i,t+150,i/2,tree.root.name)}</div>;
-                }
-            }
-            else{
-                return  <div><div style={inputstyle}><TreeNode2 id={tree.root.name}/></div> {this.getEdges(tree.left, l-i, t+150,i/2,tree.root.name)} {this.getEdges(tree.right, l+i,t+150,i/2,tree.root.name)}</div>;
-            }
-        }
-        else{
-            return ;
-        }
-      }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        var t ={
-            root:{
-                name:'a',
-                rec:false,
-            },
-            left:{
-                root:{
-                    name:'b'},
-                        left:{root:{
-                        name:'d',
-                        rec:false,},
-                        left:null,
-                        right:null,               
-                        },
-                    right:{root:{
-                        name:'e',
-                        rec:true,},
-                        left:null,
-                        right:null,               
-                    },               
-                },
-            right:{
-                root:{
-                    name:'c'},
-                        left:{root:{
-                        name:'f',
-                        rec:false,},
-                        left:null,
-                        right:null,               
-                        },
-                    right:{root:{
-                        name:'g',
-                        rec:false,},
-                        left:null,
-                        right:null,               
-                    },               
-                },
-            };
-        var w = window.innerWidth;
-        var h = window.innerHeight;
-        return(
-            <div style={{display: 'flex', justifyContent: 'space-evenly', width: '100%'}}>
-            <Xwrapper>
-            {this.getEdges(t,w/2,100,w/4, null)}
-            </Xwrapper>
-            </div>
-        );
+  getEdges(tree, l, t, i, pref) {
+    const inputstyle = {
+      position: 'absolute',
+      top: t,
+      left: l,
+    };
+    if (tree) {
+      if (pref) {
+        console.log('pref works');
+        if (tree.root.rec) {
+          return <div><div style={inputstyle}><TreeNode2 id={tree.root.name} album="favicon.png" /></div> <Xarrow start={pref} end={tree.root.name} zIndex={3} showHead={false} dashness color="gray" /> {this.getEdges(tree.left, l - i, t + 150, i / 2, tree.root.name)} {this.getEdges(tree.right, l + i, t + 150, i / 2, tree.root.name)}</div>;
+        } else {
+          return <div><div style={inputstyle}><TreeNode2 id={tree.root.name} album="favicon.png" /></div> <Xarrow start={pref} end={tree.root.name} zIndex={3} showHead={false} color="#637B47" /> {this.getEdges(tree.left, l - i, t + 150, i / 2, tree.root.name)} {this.getEdges(tree.right, l + i, t + 150, i / 2, tree.root.name)}</div>;
+        }
+      } else {
+        return <div><div style={inputstyle}><TreeNode2 id={tree.root.name} album="favicon.png" /></div> {this.getEdges(tree.left, l - i, t + 150, i / 2, tree.root.name)} {this.getEdges(tree.right, l + i, t + 150, i / 2, tree.root.name)}</div>;
+      }
+    } else {
+      return null;
     }
+  }
+
+  render() {
+    const t = {
+      root: {
+        name: 'a',
+        rec: false,
+      },
+      left: {
+        root: { name: 'b' },
+        left: {
+          root: {
+            name: 'd',
+            rec: false,
+          },
+          left: null,
+          right: null,
+        },
+        right: {
+          root: {
+            name: 'e',
+            rec: true,
+          },
+          left: null,
+          right: null,
+        },
+      },
+      right: {
+        root: { name: 'c' },
+        left: {
+          root: {
+            name: 'f',
+            rec: false,
+          },
+          left: null,
+          right: null,
+        },
+        right: {
+          root: {
+            name: 'g',
+            rec: false,
+          },
+          left: null,
+          right: null,
+        },
+      },
+    };
+    const w = window.innerWidth;
+    const h = window.innerHeight;
+    return (
+      <div style={{ display: 'flex', justifyContent: 'space-evenly', width: '100%' }}>
+        <Xwrapper>
+          {this.getEdges(this.props.tree, w / 2, 100, w / 4, null)}
+        </Xwrapper>
+      </div>
+    );
+  }
 }
 
 export default Tree;
